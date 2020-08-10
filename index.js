791c455cb1caa0e64d55a9691d8e688de483b269
Kev Wilson <kevin.wilson@bbc.co.uk>
2020-08-10 16:29:38 +0100

    Setup package.json for publishing

---
+++ index.js
@@ -0,0 +1,7 @@
+const commandBuilder = require('./src/commandBuilder')
+const Bot = require('./src/Bot')
+
+module.exports = {
+  commandBuilder,
+  Bot
+}
--- start.js
+++ start.js
@@ -2,8 +2,8 @@ const Bot = require('./src/Bot')
 const config = require('./config.json')
 const logger = require('./src/Logger')
 const commandBuilder = require('./src/commandBuilder')
-const commands = commandBuilder('commands/')
 
+const commands = commandBuilder('commands/')
 const bot = new Bot(config, commands)
 
 logger.info("Starting bot")
