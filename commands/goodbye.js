e610ae4e85e881c94bdf3362a2bf23cf08131a31
Kev Wilson <kevin.wilson@bbc.co.uk>
2020-08-11 15:59:56 +0100

    Remove implementation stuff

--- commands/goodbye.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "goodbye",
-  description: "say goodbye",
-  execute: advancedFunction
-}
-
-function advancedFunction(message, args) {
-  message.reply(`goodbye ${args[0]}`)
-}
--- commands/hello.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "hello",
-  description: "say hello",
-  execute: (message, args) => {
-    message.channel.send(`hello there ${args[0]}`)
-  }
-}
